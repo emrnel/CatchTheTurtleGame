607cf51f6e364ee3982d72615e134c07b121d458
Emre <emreilhansenel@gmail.com>
2024-07-28 12:39:50 +0300

    Added high score save logic with local file, font constant for optim, rmvd unnecessary file

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import os
 import random
 from turtle import *
 
+FONT = ('Arial', 16, "normal")
 TURTLE_SIZE = 20
 TIMER = 5
 score = 0
-high_score = 0
 game_active = False
+
+
+def get_previous_high_score():
+    fpath = os.getcwd() + "/high_score_file.txt"
+    if os.path.isfile(fpath) and os.path.getsize(fpath) > 0:
+        with open(fpath, "r") as f:
+            hs = int(f.read())
+            return hs
+    else:
+        return 0
 
 
 def check_high_score(sc):
@@ -47,33 +58,36 @@ def increase_score(x, y):
         turtle_mover(turtle)
         score += 1
         score_turtle.clear()
-        score_turtle.write(f"Score: {score}", align='center', font=('Arial', 16, "normal"))
+        score_turtle.write(f"Score: {score}", align='center', font=FONT)
 
 
 def countdown(time):
     global game_active, score
     timer_turtle.clear()
-    timer_turtle.write(f"Time: {time}", align='center', font=('Arial', 16, "normal"))
+    timer_turtle.write(f"Time: {time}", align='center', font=FONT)
     score_turtle.clear()
-    score_turtle.write(f"Score: {score}", align='center', font=('Arial', 16, "normal"))
+    score_turtle.write(f"Score: {score}", align='center', font=FONT)
     if time > 0:
         turtle_mover(turtle)
         turtle.onclick(increase_score)
-        timer_turtle.write(f"Time: {time}", align='center', font=('Arial', 16, "normal"))
+        timer_turtle.write(f"Time: {time}", align='center', font=FONT)
         my_screen.ontimer(lambda: countdown(time - 1), 1000)
     else:
         game_active = False
         score_turtle.clear()
         if check_high_score(score):
-            score_turtle.write(f"New High Score: {score}!", align='center', font=('Arial', 16, "normal"))
+            with open("high_score_file.txt", mode="w") as myFile:
+                myFile.write(f"{score}")
+            score_turtle.write(f"New High Score: {score}!", align='center', font=FONT)
         else:
-            score_turtle.write(f"Score: {score} --- High Score: {high_score}", align='center',
-                               font=('Arial', 16, "normal"))
+            score_turtle.write(f"Score: {score} --- High Score: {high_score}", align='center', font=FONT)
         timer_turtle.clear()
-        timer_turtle.write("Game Over...", align='center', font=('Arial', 16, "normal"))
-        starter_turtle.write('Press to play again:', align='center', font=('Arial', 16, "normal"))
+        timer_turtle.write("Game Over...", align='center', font=FONT)
+        starter_turtle.write('Press to play again:', align='center', font=FONT)
         starter_turtle.showturtle()
 
+
+high_score = get_previous_high_score()
 
 my_screen = Screen()
 my_screen.bgcolor('light blue')
@@ -90,7 +104,7 @@ score_turtle.penup()
 score_turtle.goto(0, my_screen.window_height() / 2 - TURTLE_SIZE * 3)
 score_turtle.pendown()
 score_turtle.color('blue')
-score_turtle.write(f"Score: {score}", align='center', font=('Arial', 16, "normal"))
+score_turtle.write(f"Score: {score}", align='center', font=FONT)
 
 turtle = Turtle()
 turtle.shape('turtle')
@@ -105,7 +119,7 @@ starter_turtle.penup()
 starter_turtle.goto(0, my_screen.window_height() / 2 - TURTLE_SIZE * 5)
 starter_turtle.pendown()
 starter_turtle.showturtle()
-starter_turtle.write('Start The Game', align='center', font=('Arial', 16, "normal"))
+starter_turtle.write('Start The Game', align='center', font=FONT)
 starter_turtle.onclick(game_starter)
 
 my_screen.mainloop()
